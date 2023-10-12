475057da126f2df6fd646e594f02fd32e4e22dbb
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-10-12 16:57:02 +0900

    classを作成した

--- main.js
+++ main.js
@@ -1,81 +1,119 @@
 "use strict"
 {
-  const canvas = document.getElementById("canvasId")
-  const context = canvas.getContext("2d")
-  canvas.width = 300
-  canvas.height = 300
-
-  let x = 0
-  let y = 75
-  let barWidth = 100
-  let barHeight = 15
-  let startingPointX = 100
-  let endPointX = startingPointX + barWidth
-  let startingPointY = 285
-  let endPointY = startingPointY + barHeight
-
-  let changeY = 10
-  let changeX = 10
-
-  //barを操作する関数
-  const movingBar = (element) => {
-    element.addEventListener("keydown", (e) => {
-      //barの残像を消す
-      context.clearRect(0, 0, canvas_beside, canvas_vertical)
-      //ballを再描画する
-      switch (e.keyCode) {
-        case 37: //左
-          startingPointX += 10
-          context.strokeRect(
-            startingPointX,
-            startingPointY,
-            barWidth,
-            barHeight
-          )
-          break
-        case 39: //右
-          startingPointX -= 10
-          context.strokeRect(
-            startingPointX,
-            startingPointY,
-            barWidth,
-            barHeight
-          )
-          break
+  class Ball {
+    constructor(_x, _y) {
+      this.canvas = document.getElementById("canvasId")
+      this.context = this.canvas.getContext("2d")
+      this.x = _x
+      this.y = _y
+      this.r = 10
+      this.changeX = 10
+      this.changeY = 10
+    }
+    update() {
+      if (this.y + this.changeY >= 300 || this.y + this.changeY <= 0) {
+        this.changeY *= -1
       }
-    })
+      this.y += this.changeY
+      if (this.x + this.changeX >= 300 || this.x + this.changeX <= 0) {
+        this.changeX *= -1
+      }
+      this.x += this.changeX
+    }
+    draw() {
+      this.context.beginPath()
+      this.context.fillStyle = "white"
+      this.context.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
+      this.context.fill()
+    }
   }
-  let intervalId = setInterval(() => {
-    //boardを描画する
-    context.fillRect(0, 0, canvas.width, canvas.height)
-    //barを描画する
-    context.strokeRect(startingPointX, startingPointY, barWidth, barHeight)
-    //barの当たり判定
-    if (y + changeY > startingPointY) {
-      changeY *= -1
-    }
-    if (
-      x + changeX > startingPointX &&
-      x + changeX > endPointX &&
-      y + changeY > startingPointY
-    ) {
-      changeX *= -1
-    }
 
-    //ボールの座標を変える
-    context.beginPath()
-    if (y + changeY >= 300 || y + changeY <= 0) {
-      changeY *= -1
+  class Bar {
+    constructor() {
+      this.canvas = document.getElementById("canvasId")
+      this.context = this.canvas.getContext("2d")
+      this.startingPointX = 100
+      this.endPointX = this.startingPointX + this.width
+      this.startingPointY = 285
+      this.endPointY = this.startingPointY + this.height
+      this.width = 100
+      this.height = 15
     }
-    y += changeY
-    if (x + changeX >= 300 || x + changeX <= 0) {
-      changeX *= -1
+    move() {
+      document.addEventListener("keydown", (e) => {
+        switch (e.keyCode) {
+          case 37: //左
+            this.startingPointX += 10
+            this.draw()
+            break
+          case 39: //右
+            this.startingPointX -= 10
+            this.draw()
+            break
+        }
+      })
     }
-    x += changeX
-    context.arc(x, y, 10, 0, 2 * Math.PI)
+    update() {
+      if (y + changeY > startingPointY) {
+        changeY *= -1
+      }
+      if (
+        x + changeX > startingPointX &&
+        x + changeX > endPointX &&
+        y + changeY > startingPointY
+      ) {
+        changeX *= -1
+      }
+    }
+    draw() {
+      this.context.fillStyle = "white"
+      this.context.fillRect(
+        this.startingPointX,
+        this.startingPointY,
+        this.width,
+        this.height
+      )
+    }
+  }
 
-    //ボールを描画する
-    context.strokeStyle = "white"
-    context.stroke()
-  }, 100)
+  class Board {
+    constructor() {
+      this.canvas = document.getElementById("canvasId")
+      this.context = this.canvas.getContext("2d")
+      this.ball = new Ball(100, 75) //決めうちの値を後で修正
+      this.bar = new Bar()
+      this.width = 300
+      this.height = 300
+      this.intervalId
+    }
+    set() {
+      this.intervalId = setInterval(() => {
+        this.update()
+        this.draw()
+      }, 100)
+    }
+    update() {
+      this.ball.update()
+      //barの判定
+      if (this.ball.y + this.ball.changeY > this.bar.startingPointY) {
+        this.ball.changeY *= -1
+      }
+      if (
+        this.ball.x + this.ball.changeX > this.bar.startingPointX &&
+        this.ball.x + this.ball.changeX > this.bar.endPointX &&
+        this.ball.y + this.ball.changeY > this.bar.startingPointY
+      ) {
+        this.ball.changeX *= -1
+      }
+    }
+    draw() {
+      this.context.fillRect(0, 0, this.width, this.height)
+      this.ball.draw()
+      this.bar.draw()
+    }
+  }
+
+  const board = new Board()
+  board.set()
+  board.draw()
 }
