069c3e2caca5f896c70a1d5bfa83d9989e708171
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-10-16 17:57:01 +0900

    スコアが5上がるごとにボールのスピードを上げる実装を追加

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
   canvas.width = 300
   canvas.height = 300
   const score = document.createElement("div")
-  const HP = document.createElement("div")
+  // const HP = document.createElement("div")
   score.textContent = `score: 0`
-  HP.textContent = `HP: 3`
+  // HP.textContent = `HP: 3`
   document.body.appendChild(score)
-  document.body.appendChild(HP)
+  // document.body.appendChild(HP)
 
   //引数の範囲でランダムな数を返す関数
   const rand = (min, max) => {
@@ -26,8 +26,8 @@
       this.x = _x
       this.y = _y
       this.r = 10
-      this.changeX = 10
-      this.changeY = 10
+      this.changeX = 1
+      this.changeY = 1
     }
     update() {
       this.x += this.changeX
@@ -38,6 +38,10 @@
     }
     changeDirectionY() {
       this.changeY *= -1
+    }
+    speedUp() {
+      this.changeX += this.changeX > 0 ? 1 : -1
+      this.changeY += this.changeY > 0 ? 1 : -1
     }
     draw() {
       this.context.beginPath()
@@ -118,9 +122,8 @@
       this.width = 300
       this.height = 300
       this.intervalId
-      this.speed = 100
-      this.hitCount = 0
-      this.HP = 3
+      this.speed = 15
+      this.score = 0
       this.gameOver = false
       this.init()
     }
@@ -139,14 +142,10 @@
         }
         this.bar.draw()
       })
+      this.adjustAngle()
     }
     set() {
       this.intervalId = setInterval(() => {
-        if (this.hitCount > 10) {
-          this.speed = 60
-          clearInterval(this.intervalId)
-          this.set()
-        }
         this.update()
         this.draw()
       }, this.speed)
@@ -154,31 +153,33 @@
     update() {
       //barの判定
       if (this.bar.isHit(this.ball) === true) {
-        this.ball.changeY *= -1
-      }
-      if (HP == 0) {
-        clearInterval(this.intervalId)
-        alert("game over")
+        this.ball.changeDirectionY()
       }
       this.ball.update()
-      this.isHit()
-      this.adjustAngle()
-      score.textContent = `score:${this.hitCount}`
-      HP.textContent = `HP:${this.HP}`
+      if (this.isHit() == true) {
+        //scoreが５ずつ変わるごとにスピードアップする
+        if (this.score % 5 == 0) {
+          this.ball.speedUp()
+        }
+      }
+      score.textContent = `score:${this.score}`
     }
     isHit() {
+      let count = this.score
       if (this.ball.x > canvas.width || this.ball.x < 0) {
         this.ball.changeDirectionX()
-        this.hitCount++
+        this.score++
       }
       if (this.ball.y < 0) {
         this.ball.changeDirectionY()
-        this.hitCount++
+        this.score++
       }
       if (this.ball.y > canvas.height) {
-        this.HP--
-        this.hitCount = 0
+        this.score = 0
         clearInterval(this.intervalId)
+      }
+      if (count !== this.score) {
+        return true
       }
     }
     adjustAngle() {
@@ -189,6 +190,18 @@
           this.ball.changeY = 15
         }
       })
+    }
+    KeyDown(e) {
+      if (e.keyCode == 39) {
+        return (this.pressedKey = "left")
+      } else if (e.keyCode == 37) {
+        return (this.pressedKey = "right")
+      }
+    }
+    KeyUp(e) {
+      if (e.keyCode == 39 || e.keyCode == 37) {
+        return (this.pressedKey = undefined)
+      }
     }
     draw() {
       this.context.fillStyle = "black"
